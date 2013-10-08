ea8b3706533a117dfb4cb601ebc53f879bb5066c
hubicoder2 <pooh@poohs-iMac.local>
2013-10-08 16:13:38 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '02dde62334973b9f08941203ba6a821f0641d1525eaec898c56da1c7a626d239d423051c88bb061dc92ca101e633339953f95b3c7c3b092ce8689dfb50943216'
