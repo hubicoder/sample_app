613eec655e234b0f1313303f5f05179078848f0d
hubicoder2 <pooh@poohs-iMac.local>
2013-10-11 03:42:53 +0900

    layout done

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.2.7'
 gem 'sqlite3'
 gem 'rspec-rails'
 gem 'guard-rspec'
+gem 'bootstrap-sass'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,21 +2,23 @@ require 'spec_helper'
 
 describe "StaticPages" do
   describe "Home page" do
-    it "should have the constant 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_content('Sample App')
-    end
-
     it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      page.should have_selector('title', :text => " | Home")
+      visit home_path
+      page.should have_selector('title', text: " | Home")
     end
   end
 
   describe "About Us.." do
     it "should have the constant 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_content('About')
+    end
+  end
+
+  describe "Contact.." do
+    it "should have the constant 'Contact'" do
+      visit contact_path
+      page.should have_content('Contact')
     end
   end
 end
